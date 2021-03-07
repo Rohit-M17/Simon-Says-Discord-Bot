7bd607b411dda498678984b7e578ef7962a18555
Dylan Riffel <driff001@hammer.cs.ucr.edu>
2021-03-07 14:47:59 -0800

    Got the Queue function working

--- SimonSays.py
+++ SimonSays.py
@@ -1,9 +1,5 @@
-
-
-
-
-
 import discord
+import asyncio
 from discord.ext import commands,tasks
 from discord.voice_client import VoiceClient
 import youtube_dl
@@ -37,7 +33,7 @@ ffmpeg_options = {
 ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 
 class YTDLSource(discord.PCMVolumeTransformer):
-    def __init__(self, source, *, data, volume=0.5):
+    def __init__(self, source, *, data, volume=1.0):
         super().__init__(source, volume)
 
         self.data = data
@@ -49,7 +45,7 @@ class YTDLSource(discord.PCMVolumeTransformer):
     @classmethod
 
     async def from_url(cls, url, *, loop=None, stream=False):
-
+        
         loop = loop or asyncio.get_event_loop()
         data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
 
@@ -63,81 +59,120 @@ class YTDLSource(discord.PCMVolumeTransformer):
 
 bot = commands.Bot(command_prefix='!')
 
+
 mood = ['Vibing to music','Looking for some new Heat','AFK','In the Stu']
 
 Queue = []
-
-
-
+Rqueue = []
+   
+      
 @bot.event
 async def on_ready():
 	change_mood.start()
 	print('Simon is online')
 
-@bot.command()
+@bot.command(name='join', help='Joins the user voice channel')
 async def join(ctx):
-   
-    channel = ctx.author.voice.channel
-    await channel.connect()
-
-
-@bot.command(name='play', help='Plays songs directly from Youtube and Soundcloud')
-async def play(ctx, url):
-    if ctx.message.author.voice:
-    	await ctx.send('You must connect to a voice channel to connect')
-    else:
-    global queue
-
-    queue.append(url)
-
-    channel = ctx.author.voice.channel
-    await channel.connect()
-
-    server = ctx.message.guild
-    voice_channel = server.voice_client
+	if not ctx.message.author.voice:
+		await ctx.send("You are not in a Voice Channel")
+		return
     
-    async with ctx.typing():
+	else:
+		voice_channel = ctx.message.author.voice.channel
 
-        player = await YTDLSource.from_url(url, loop=bot.loop)
-        voice_channel.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
+	await voice_channel.connect()
 
-    await ctx.send('**Playing:** {}'.format(player.title))
-    
+@bot.command(name='play', help='Plays a song immediately')
+async def play(ctx,url):
+	server = ctx.message.guild
+	voice_channel = server.voice_client   
+	Queue.append(url)
+	voice_channel.stop()
+	async with ctx.typing():
+		player = await YTDLSource.from_url(url,  loop = bot.loop)
+		Rqueue.append(player.title)
+	await ctx.send("**Added:** " + player.title + "** to the Queue**")
+	while True:
+		await asyncio.sleep(1)
+		if voice_channel.is_playing() == False:
+			
+			voice_channel.play(player, after=lambda e: Rqueue.pop(0))
+    		
+	
 
-@bot.command(name='queue', help='Adds a song to the queue')
-async def queue_(ctx):
-    await ctx.send(f'Queue: `{queue}`')
+@bot.command(name='enqueue', help='Adds a song to the queue')
+async def enqueue(ctx,url):
+	server = ctx.message.guild
+	voice_channel = server.voice_client   
+	Queue.append(url)
+	async with ctx.typing():
+		player = await YTDLSource.from_url(url,  loop = bot.loop)
+		Rqueue.append(player.title)
+	await ctx.send("**Added:** " + player.title + "** to the Queue**")
+	while True:
+		await asyncio.sleep(5)
+		if voice_channel.is_playing() == False:
+			voice_channel.play(player, after=lambda e: Rqueue.pop(0))
+			
+	
+	
 
-@bot.command(name='skip', help='This command skips the song')
-async def skip(ctx):
-    server = ctx.message.guild
-    
-    voice_channel = server.voice_client
+@bot.command(name='queue', help='Shows all songs in the queue')
+async def queue(ctx):
+	if Rqueue == []:
+		await ctx.send('**Nothing is in the Queue**')
+	else:
+		await ctx.send(f'Queue:\n')
+		for number, letter in enumerate(Rqueue):	
+			await ctx.send(f'`{number+1}.{letter}`')    
 
-    voice_channel.stop()
+@bot.command(name='np', help='Shows the current song')
+async def np(ctx):
+	server = ctx.message.guild
+	voice_channel = server.voice_client 
+	if voice_channel.is_playing() == False:
+		await ctx.send('**Nothing is Playing**')
+	else:
+		await ctx.send(Rqueue[0] + " **is now playing**"	)
 
-@bot.command(name='pause', help='This command pauses the given song')
+
+
+
+@bot.command(name='pause', help='This command pauses the song')
 async def pause(ctx):
 	server = ctx.message.guild
     
-    voice_channel = server.voice_client
+	voice_channel = server.voice_client
 
-    voice_channel.pause()
-
-@bot.command(name='resume', help='This command resumes a paused song')
+	voice_channel.pause()
+@bot.command(name='resume', help='This command resumes the song')
 async def resume(ctx):
 	server = ctx.message.guild
     
-    voice_channel = server.voice_client
+	voice_channel = server.voice_client
 
-    voice_channel.resume()
+	voice_channel.resume()
+@bot.command(name='skip', help='This command skips the song')
+async def skip(ctx):
+	server = ctx.message.guild
+    
+	voice_channel = server.voice_client
 
+	voice_channel.stop()
+
+@bot.command(name='disconnect', help='This command makes the bot leave the voice channel')
+async def disconnect(ctx):
+	voice_channel = ctx.message.guild.voice_client
+	await voice_channel.disconnect()
 
 @tasks.loop(seconds=180)
 async def change_mood():
 	await bot.change_presence(activity=discord.Game(choice(mood)))	
+@tasks.loop(seconds=180)
+async def change_mood():
+	await bot.change_presence(activity=discord.Game(choice(mood)))	
 
-bot.run('ODEzMTgxMTg4OTU3MDEyMDE4.YDLj_w.o9GJQG75-P9lV5vs-23AhkedAOU')
+bot.run('token')
 
 
 
