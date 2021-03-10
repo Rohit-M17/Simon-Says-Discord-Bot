35be6e810262b60faa9ad7674a401342ccde0822
Rohit-M17 <rohit.manimaran@gmail.com>
2021-03-10 03:10:49 -0800

    This is the latest implementation of play function for Spotify Songs

--- SimonSays.py
+++ SimonSays.py
@@ -4,27 +4,49 @@ from discord.ext import commands,tasks
 from discord.voice_client import VoiceClient
 from discord.utils import get 
 import youtube_dl
+import spotify_dl
 import os
 from os import system
-
-
 from random import choice
 
-youtube_dl.utils.bug_reports_message = lambda: ''
+TOKEN = 
+BOT_PREFIX = '!'
+bot = commands.Bot(command_prefix = BOT_PREFIX)
 
-ytdl_format_options = {
-    'format': 'bestaudio/best',
-    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
-    'restrictfilenames': True,
-    'noplaylist': True,
-    'nocheckcertificate': True,
-    'ignoreerrors': False,
-    'logtostderr': False,
-    'quiet': True,
-    'no_warnings': True,
-    'default_search': 'auto',
-    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
-}
+# youtube_dl.utils.bug_reports_message = lambda: ''
+
+# ytdl_format_options = {
+#     'format': 'bestaudio/best',
+#     'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+#     'restrictfilenames': True,
+#     'noplaylist': True,
+#     'nocheckcertificate': True,
+#     'ignoreerrors': False,
+#     'logtostderr': False,
+#     'quiet': True,
+#     'no_warnings': True,
+#     'default_search': 'auto',
+#     'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+# }
+
+# sptdl_format_options = {
+#     'format': 'bestaudio/best',
+#     'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+#     'restrictfilenames': True,
+#     'noplaylist': True,
+#     'nocheckcertificate': True,
+#     'ignoreerrors': False,
+#     'logtostderr': False,
+#     'quiet': True,
+#     'no_warnings': True,
+#     'default_search': 'auto',
+#     'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+# }
+
+
+
+
+
 
 ffmpeg_options = {
     'options': '-vn'
@@ -34,11 +56,12 @@ ffmpeg_options = {
 
 
 
-ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+# ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+# sptdl = youtube_dl.SpotifyDL(sptdl_format_options)
 
 
 
-bot = commands.Bot(command_prefix='!')
+# bot = commands.Bot(command_prefix='!')
 
 
 mood = ['Vibing to music','Looking for some new Heat','AFK','In the Stu']
@@ -47,6 +70,7 @@ mood = ['Vibing to music','Looking for some new Heat','AFK','In the Stu']
 Queue = []
 Rqueue = []
 
+CurSong = []
 
 @bot.event
 async def on_ready():
@@ -120,44 +144,34 @@ class Factory(discord.PCMVolumeTransformer):
     @classmethod
 
     async def get_url(cls, url, *, loop=None, stream=False):
-        
         loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
-
-        if 'entries' in data:
-            # take first item from a playlist
-            data = data['entries'][0]
-
-        filename = data['url'] if stream else ytdl.prepare_filename(data)
-        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
-   
-##////////////////////////////////////////////////////////////////////////////////////////////////////     
+        system("spotdl " + url)
+        for file in os.listdir("./"):
+            if file.endswith(".mp3"):
+                name = file
+                print(f"Renamed file: {file}")
+                os.rename(file, "currentsong.mp3")
+        return name
 
 class Song:
-
-	CurSong = []
-
-
-
-
-	@bot.command(name='play', help='Plays a song immediately')
-	async def play(ctx,url):
-
-		server = ctx.message.guild
-		voice_channel = server.voice_client   
-		Queue.append(url)
-		voice_channel.stop()
-		async with ctx.typing():
-			player = await Factory.get_url(url,  loop = bot.loop)
-			Rqueue.append(player.title)
-		await ctx.send("**Added:** " + player.title + "** to the Queue**")
-		while True:
-			await asyncio.sleep(1)
-			if voice_channel.is_playing() == False:
-				
-				voice_channel.play(player, after=lambda e: Rqueue.pop(0))
-	    		
-		
+    @bot.command(name='play', help='Plays a song immediately')
+    async def play(ctx,url):
+        server = ctx.message.guild
+        global voice_channel
+        voice_channel=server.voice_client
+        Queue.append(url)
+        voice_channel.stop()
+        async with ctx.typing():
+            player = await Factory.get_url(url, loop = bot.loop)
+            Rqueue.append(player.title)
+            
+        await ctx.send("**Added:** " + "Spotify Song" + "** to the Queue**")
+        while True:
+            await asyncio.sleep(1)
+            if voice_channel.is_playing() == False:
+                voice_channel.play(discord.FFmpegPCMAudio("currentsong.mp3"), after=lambda e: print(f"{player.title} has finished playing"))
+                voice_channel.source = discord.PCMVolumeTransformer(voice_channel.source)
+                voice_channel.source.volume = 0.5
 class Playlist:
 	@bot.command(name='enqueue', help='Adds a song to the queue')
 	async def enqueue(ctx,url:str):	 
@@ -209,11 +223,4 @@ async def queue(ctx):
 async def change_mood():
 	await bot.change_presence(activity=discord.Game(choice(mood)))	
 
-bot.run('ODEzMTgxMTg4OTU3MDEyMDE4.YDLj_w.o9GJQG75-P9lV5vs-23AhkedAOU')
-
-
-
-
-
-
-
+bot.run(TOKEN)
